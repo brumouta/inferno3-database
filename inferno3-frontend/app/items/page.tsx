24ecfbb718388a97fbddc0393485a05c44435930
Bruno Mouta <brumouta@gmail.com>
2023-10-22 21:29:54 +0100

    Fix search params on items

--- inferno3-frontend/app/items/page.tsx
+++ inferno3-frontend/app/items/page.tsx
@@ -1,14 +1,18 @@
+'use client'
 import {ItemsTable} from "@/components/ItemsTable";
 import {get} from "@/lib/api";
 import {Item} from "@/types/item";
+import {useSearchParams} from "next/navigation";
 
 
-export default async function Items({ searchParams }: { area: string }) {
+export default async function Items() {
+  const searchParams = useSearchParams()!
+
   const res = await get<Item[]>(
       `${process.env.BACKEND_URL}/v1/items`
   );
 
-  const quickFilter = searchParams && searchParams.area ? searchParams.area : "";
+  const quickFilter: string = searchParams && searchParams.get('area') ? searchParams.get('area')! : "";
 
   return (
     <ItemsTable items={res.data} quickFilter={quickFilter} ></ItemsTable>
